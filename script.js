7536194fd4f9b8a68a8e7e815870f9efc952f57d
Joshua King <joshuaking85@gmail.com>
2015-10-05 21:15:34 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+menuElement = document.getElementById("menu");
+        console.log("got the menu element");
+        
+        //menuElement.innerHTML = "This is the new menu text";
+        
+        function HandleMenuClick(){
+            $('#menu').html('This is the new menu text');
+        }
